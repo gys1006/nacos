68f32447e9476f7e55d4563a6c46f73460d37836
gys <gys>
2021-09-23 18:11:47 +0800

    nacos注册测试

--- nacosTest/provide/src/main/java/com/nacos/test/test.java
+++ nacosTest/provide/src/main/java/com/nacos/test/test.java
@@ -1,6 +1,9 @@
 package com.nacos.test;
 
 
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class test {
 
+
+    @Value("${db.host:host}")
+    private String useLocalCache;
+
+   /* public void setUseLocalCache(String useLocalCache) {
+        this.useLocalCache = useLocalCache;
+    }*/
     @GetMapping("test")
     @ResponseBody
     public String get(){
+        System.out.println(useLocalCache);
         return "provideNacos";
     }
 }
