4952a6777c9c3742118b409d9fc6cee0620ecb14
gys <gys>
2021-09-25 17:10:24 +0800

    规划端口号

--- nacosTest/consume/consume-server/src/main/java/com/nacos/test/ConsumeController.java
+++ nacosTest/consume/consume-server/src/main/java/com/nacos/test/ConsumeController.java
@@ -4,16 +4,12 @@ package com.nacos.test;
 import com.nacos.feign.ConsumerFeign;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
 
 @RestController
 @RefreshScope
