afb380802fbb2dfd0f0a1a71a508b9f3352002a1
gys <gys>
2021-09-28 14:28:40 +0800

    RocketMq

--- nacosTest/application/src/main/java/com/application/SpringApplication.java
+++ nacosTest/application/src/main/java/com/application/SpringApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class SpringApplication {
     public static void main(String[] args) {
         org.springframework.boot.SpringApplication.run(SpringApplication.class, args);
--- nacosTest/application/src/main/java/com/application/controller/FeigtController.java
+++ nacosTest/application/src/main/java/com/application/controller/FeigtController.java
@@ -2,7 +2,6 @@ package com.application.controller;
 
 import com.nacos.ProvideApi;
 import org.apache.dubbo.config.annotation.Reference;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class FeigtController {
 
 
-    @Reference(retries = 0,check = false)
+    @Reference(retries = 3,check = false)
     ProvideApi provideApi;
 
     @GetMapping("feigtTest")
